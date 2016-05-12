5202bb089ac5a261a01831cd6454e8fd986190d7
lykmapipo <lallyelias87@gmail.com>
2016-05-12 17:29:30 +0300

    refactor Mail.send to allow Mail.queue

--- spec/unit.spec.js
+++ spec/unit.spec.js
@@ -2,6 +2,7 @@
 
 //dependencies
 var path = require('path');
+var async = require('async');
 var expect = require('chai').expect;
 var faker = require('faker');
 var Mail = require(path.join(__dirname, '..'));
@@ -27,7 +28,7 @@ describe('byteskode mailer', function() {
     });
 
 
-    it('should be able to log email in test and development mode', function(done) {
+    it('should be able to send email in test and development mode', function(done) {
 
         var email = {
             recipientName: faker.name.findName(),
@@ -65,6 +66,62 @@ describe('byteskode mailer', function() {
 
                 done(error, response);
             });
+    });
+
+
+    it('should be able to resend email(s) in test and development mode', function(done) {
+
+        var email = {
+            recipientName: faker.name.findName(),
+            token: faker.random.uuid(),
+            to: faker.internet.email(),
+            baseUrl: faker.internet.url(),
+            subject: 'Account confirmation',
+            html: faker.lorem.sentence(),
+            sender: faker.internet.email(),
+            from: faker.internet.email()
+        };
+
+        async.waterfall([
+
+            function createMail(next) {
+                Mail.create(email, next);
+            },
+
+            function resend(mail, next) {
+                Mail.resend(next);
+            }
+
+        ], function(error, response) {
+
+            expect(error).to.not.exist;
+
+            expect(response).to.exist;
+
+            response = response[0];
+
+            expect(response.type).to.exist;
+            expect(response.type).to.be.equal('Normal');
+            expect(response.type).to.be.a.String;
+
+            expect(response.to).to.exist;
+            expect(response.to).to.be.an.Array;
+            expect(response.to).to.include(email.to);
+
+            expect(response.sentAt).to.exist;
+            expect(response.sentAt).to.be.a.Date;
+
+            expect(response.html).to.exist;
+            expect(response.html).to.be.a.String;
+
+            expect(response.subject).to.exist;
+            expect(response.subject).to.be.equal('Account confirmation');
+            expect(response.subject).to.be.a.String;
+
+
+            done(error, response);
+        });
+
     });
 
 });
